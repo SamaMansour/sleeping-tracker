8df7f64de831daf8a0a84bf94f9cbfa06c2b2ba5
SamaMansour <sma302000@gmail.com>
2023-02-06 16:49:26 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import Register from './layouts/auth/register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import "./App.css"
 function App() {
   return (
+
+
+    <>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/signup">Signup</Nav.Link>
+          <Nav.Link href="#">Login</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+ 
    
+
     <BrowserRouter>
       <Routes>
         <Route path='/signup' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
+  </>
+   
+    
    
   );
 }
